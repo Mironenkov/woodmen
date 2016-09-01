2113927b0915066e461549f9e8d93ef8e5b9e4ff
Mironenkov Ruslan <mironenkovr@gmail.com>
2016-09-01 12:20:30 +0300

    Add card_item.html

--- scripts/common.js
+++ scripts/common.js
@@ -2,5 +2,19 @@
  * Created by GloryManU on 04.07.2016.
  */
 (function(){
-	
+	$('.wm-description_card-slider').lightSlider({
+        gallery:true,
+        item:1,
+        loop:true,
+        slideMargin:0,
+        enableDrag: false,
+        galleryMargin: 2,
+        thumbMargin: 3,
+        currentPagerPosition:'left',
+        onSliderLoad: function(el) {
+            el.lightGallery({
+                selector: '.wm-description_card-slider .lslide'
+            });
+        }
+    });
 })();
